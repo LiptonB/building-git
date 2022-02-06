e5a5e92a5204dc0064b0f6101b157034610484b6
Ben Lipton <ben.lipton@gmail.com>
2022-02-06 11:37:18 -0500

    Add unit tests for ChecksummedFile

--- src/index/checksum.rs
+++ src/index/checksum.rs
@@ -71,3 +71,80 @@ impl<I: Read, D: Digest> Read for ChecksummedFile<I, D> {
         Ok(out)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+    use std::path::PathBuf;
+    use std::io::{Read, Write};
+
+    use crypto::sha1::Sha1;
+    use tempfile::{tempdir, TempDir};
+
+    use super::ChecksummedFile;
+
+    struct Fixture {
+        tempdir: TempDir
+    }
+
+    impl Fixture {
+        fn new() -> Self {
+            let tempdir = tempdir().expect("tempdir");
+
+            Self {
+                tempdir
+            }
+        }
+
+        fn get_filename(&self) -> PathBuf {
+            let mut filename = PathBuf::from(&self.tempdir.path());
+            filename.push("testfile");
+
+            filename
+        }
+
+        fn create_file(&self) -> ChecksummedFile<File, Sha1> {
+            let filename = self.get_filename();
+            let file = File::create(&filename).expect("File::create");
+            ChecksummedFile::new(file, Sha1::new())
+        }
+
+        fn open_file(&self) -> ChecksummedFile<File, Sha1> {
+            let filename = self.get_filename();
+            let file = File::open(&filename).expect("File::open");
+            ChecksummedFile::new(file, Sha1::new())
+        }
+    }
+
+    #[test]
+    fn can_write_checksummed_file() {
+        let fixture = Fixture::new();
+
+        {
+            let mut file = fixture.create_file();
+            file.write_all(b"test_contents").expect("write_all");
+            file.write_hash().expect("write_hash");
+        }
+
+        let expected = b"test_contents\x57\xc5\x84\x76\x41\xe1\xac\xef\xc8\xf9\xeb\xe8\x1d\x21\x13\x0b\xfa\x0c\x75\x54";
+        let actual = std::fs::read(&fixture.get_filename()).expect("read");
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn can_read_checksummed_file() {
+        let fixture = Fixture::new();
+
+        {
+            let mut file = fixture.create_file();
+            file.write_all(b"test_contents").expect("write_all");
+            file.write_hash().expect("write_hash");
+        }
+
+        let mut file = fixture.open_file();
+        let mut data: [u8; 13] = [0; 13];
+        file.read_exact(&mut data).expect("read_exact");
+        assert_eq!(&data, b"test_contents");
+        assert!(file.verify_checksum().expect("verify_checksum"));
+   }
+}
