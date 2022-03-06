4a7eb4ccaa5917c0605a1dc11a365f14c03ed41a
Ben Lipton <ben.lipton@gmail.com>
2022-03-06 12:16:17 -0500

    Move cli stuff into cmd module

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,3 +1,28 @@
-pub mod add;
-pub mod commit;
-pub mod init;
+mod add;
+mod commit;
+mod init;
+
+use std::ffi::OsString;
+
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+enum Cli {
+    Init(init::Args),
+    Commit(commit::Args),
+    Add(add::Args),
+}
+
+pub fn execute<I, T>(args: I) -> Result<()>
+where
+    I: IntoIterator<Item = T>,
+    T: Into<OsString> + Clone,
+{
+    let opt = Cli::parse_from(args);
+    match opt {
+        Cli::Init(args) => init::execute(args),
+        Cli::Commit(args) => commit::execute(args),
+        Cli::Add(args) => add::execute(args),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,10 @@ mod telemetry;
 mod workspace;
 
 use anyhow::Result;
-use clap::Parser;
-
-#[derive(Parser, Debug)]
-enum Cli {
-    Init(cmd::init::Args),
-    Commit(cmd::commit::Args),
-    Add(cmd::add::Args),
-}
 
 fn main() -> Result<()> {
     telemetry::init();
 
-    let opt = Cli::parse();
-    match opt {
-        Cli::Init(args) => cmd::init::execute(args)?,
-        Cli::Commit(args) => cmd::commit::execute(args)?,
-        Cli::Add(args) => cmd::add::execute(args)?,
-    }
-
-    Ok(())
+    let args = std::env::args_os();
+    cmd::execute(args)
 }
